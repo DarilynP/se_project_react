6c86ccf30b4b28802f41d866378db23b181a48a5
Darilyn <darilyn.peguero@gmail.com>
2025-03-02 17:04:40 -0500

    corrected BEM, added alt description

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,15 +1,15 @@
 import "./ItemModal.css";
 
-function ItemModal({ activeModal, onClose, card }) {
+function ItemModal({ activeModal, onClose, card = { name: "Image preview" } }) {
   return (
-    <div className={`modal ${activeModal === "preview" && "modal__opened"}`}>
+    <div className={`modal ${activeModal === "preview" ? "modal__opened" : ""}`}>
       <div className="modal__content modal__content_type_image">
         <button
           onClick={onClose}
           type="button"
           className="modal__close modal__close-item"
         ></button>
-        <img src={card.link} alt="" className="modal__image" />
+        <img src={card.link} alt={card.name} className="modal__image" />
 
         <div className="modal__footer">
           {/*
